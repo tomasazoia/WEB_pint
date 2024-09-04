0521ec588cbd07929dcd080317ef9b1b9d2e91ad
Tomas Azoia <tomasazoiamorais@gmail.com>
2024-09-04 17:46:09 +0100

    navbar

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -38,128 +38,122 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav class="navbar bg-body-tertiary fixed-top">
-      <div class="container-fluid">
-      <a href="/dashboard">
-  <img src="/logotipo-softinsa.png" alt="Logotipo Softinsa" class="navbar-logo" />
-</a>
-<button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+    <nav className="navbar bg-body-tertiary fixed-top">
+      <div className="container-fluid">
+        <Link to="/dashboard">
+          <img src="/logotipo-softinsa.png" alt="Logotipo Softinsa" className="navbar-logo" />
+        </Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-          <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
-          <div class="offcanvas-body">
-            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-              <li class="nav-item">
-                <a class="nav-link active text-secondary fs-6" href="/dashboard">Dashboard</a>
+          <div className="offcanvas-body">
+            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li className="nav-item">
+                <Link className="nav-link active text-secondary fs-6" to="/dashboard">Dashboard</Link>
               </li>
-              <li class="nav-item">
-                <a class="nav-link text-secondary fs-6" href="/calendario">Calendário</a>
+              <li className="nav-item">
+                <Link className="nav-link text-secondary fs-6" to="/calendario">Calendário</Link>
               </li>
-              <li class="nav-item">
-                <a class="nav-link text-secondary fs-6" href="/formularios">Formulários</a>
+              <li className="nav-item">
+                <Link className="nav-link text-secondary fs-6" to="/formularios">Formulários</Link>
               </li>
-              <li class="nav-item">
-                <a class="nav-link text-secondary fs-6" href="/notificacoes">Notificações</a>
+              <li className="nav-item">
+                <Link className="nav-link text-secondary fs-6" to="/notificacoes">Notificações</Link>
               </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-secondary fs-6" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Utilizadores
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="userDropdown">
-                  <li><a class="dropdown-item" href="/user/list">Gerir Utilizadores Validados</a></li>
-                  <li><a class="dropdown-item" href="/user/listNVal">Listar Utilizadores por Validar</a></li>
-                </ul>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-secondary fs-6" href="#" id="reportsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Reports em Comentarios
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="reportsDropdown">
-                  <li><a class="dropdown-item" href="/reportEventos">Eventos</a></li>
-                  <li><a class="dropdown-item" href="/reportLocais">Locais</a></li>
-                </ul>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link text-secondary fs-6" href="/centro/list">Centros</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-secondary fs-6" href="#" id="comDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Validar Comentários
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="comDropdown">
-                  <li><a class="dropdown-item" href="/comentariosinv">Eventos</a></li>
-                  <li><a class="dropdown-item" href="/comentariosinvlocal">Locais</a></li>
-                  <li><a class="dropdown-item" href="/validarcomfor">Foruns</a></li>
-                </ul>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-secondary fs-6" href="#" id="locaisDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Estabelecimentos
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="locaisDropdown">
-                  <li><a class="dropdown-item" href="/locais/list">Gerir Estabelecimentos Validados</a></li>
-                  <li><a class="dropdown-item" href="/locais/validarlocais">Listar Estabelecimentos por Validar</a></li>
-                </ul>
-              </li>
-
               <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle text-secondary fs-6" href="#" id="eventosDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Eventos
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="eventosDropdown">
-
-                  <li><a class="dropdown-item" href="/evento/manage">Gerir Eventos</a></li>
-                  <li><a class="dropdown-item" href="/evento/list">Listar Eventos Disponíveis</a></li>
+                <Link className="nav-link dropdown-toggle text-secondary fs-6" to="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Utilizadores
+                </Link>
+                <ul className="dropdown-menu" aria-labelledby="userDropdown">
+                  <li><Link className="dropdown-item" to="/user/list">Gerir Utilizadores Validados</Link></li>
+                  <li><Link className="dropdown-item" to="/user/listNVal">Listar Utilizadores por Validar</Link></li>
                 </ul>
               </li>
-
+              <li className="nav-item dropdown">
+                <Link className="nav-link dropdown-toggle text-secondary fs-6" to="#" id="reportsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Reports em Comentarios
+                </Link>
+                <ul className="dropdown-menu" aria-labelledby="reportsDropdown">
+                  <li><Link className="dropdown-item" to="/reportEventos">Eventos</Link></li>
+                  <li><Link className="dropdown-item" to="/reportLocais">Locais</Link></li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-secondary fs-6" to="/centro/list">Centros</Link>
+              </li>
+              <li className="nav-item dropdown">
+                <Link className="nav-link dropdown-toggle text-secondary fs-6" to="#" id="comDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Validar Comentários
+                </Link>
+                <ul className="dropdown-menu" aria-labelledby="comDropdown">
+                  <li><Link className="dropdown-item" to="/comentariosinv">Eventos</Link></li>
+                  <li><Link className="dropdown-item" to="/comentariosinvlocal">Locais</Link></li>
+                  <li><Link className="dropdown-item" to="/validarcomfor">Foruns</Link></li>
+                </ul>
+              </li>
+              <li className="nav-item dropdown">
+                <Link className="nav-link dropdown-toggle text-secondary fs-6" to="#" id="locaisDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Estabelecimentos
+                </Link>
+                <ul className="dropdown-menu" aria-labelledby="locaisDropdown">
+                  <li><Link className="dropdown-item" to="/locais/list">Gerir Estabelecimentos Validados</Link></li>
+                  <li><Link className="dropdown-item" to="/locais/validarlocais">Listar Estabelecimentos por Validar</Link></li>
+                </ul>
+              </li>
+              <li className="nav-item dropdown">
+                <Link className="nav-link dropdown-toggle text-secondary fs-6" to="#" id="eventosDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Eventos
+                </Link>
+                <ul className="dropdown-menu" aria-labelledby="eventosDropdown">
+                  <li><Link className="dropdown-item" to="/evento/manage">Gerir Eventos</Link></li>
+                  <li><Link className="dropdown-item" to="/evento/list">Listar Eventos Disponíveis</Link></li>
+                </ul>
+              </li>
               {/* Áreas */}
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-secondary fs-6" href="#" id="areaDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <li className="nav-item dropdown">
+                <Link className="nav-link dropdown-toggle text-secondary fs-6" to="#" id="areaDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Áreas
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="areaDropdown">
+                </Link>
+                <ul className="dropdown-menu" aria-labelledby="areaDropdown">
                   {areas.map(area => (
                     <li key={area.ID_AREA}>
-                      <a class="dropdown-item" href={`/locais/listarea/${area.ID_AREA}`}>{area.NOME_AREA}</a>
+                      <Link className="dropdown-item" to={`/locais/listarea/${area.ID_AREA}`}>{area.NOME_AREA}</Link>
                     </li>
                   ))}
-                  <div class="dropdown-item create-btn">
-                    <a href="/area/create" class="btn btn-primary w-100">Gerir Áreas</a>
+                  <div className="dropdown-item create-btn">
+                    <Link to="/area/create" className="btn btn-primary w-100">Gerir Áreas</Link>
                   </div>
                 </ul>
               </li>
-
               {/* Sub-Áreas */}
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-secondary fs-6" href="#" id="subareaDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <li className="nav-item dropdown">
+                <Link className="nav-link dropdown-toggle text-secondary fs-6" to="#" id="subareaDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Sub-Áreas
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="subareaDropdown">
+                </Link>
+                <ul className="dropdown-menu" aria-labelledby="subareaDropdown">
                   {subareas.map(subarea => (
                     <li key={subarea.ID_SUB_AREA}>
-                      <a class="dropdown-item" href={`/locais/listsubarea/${subarea.ID_SUB_AREA}`}>{subarea.NOME_SUBAREA}</a>
+                      <Link className="dropdown-item" to={`/locais/listsubarea/${subarea.ID_SUB_AREA}`}>{subarea.NOME_SUBAREA}</Link>
                     </li>
                   ))}
-                  <div class="dropdown-item create-btn">
-                    <a href="/subarea/create" class="btn btn-primary w-100">Gerir Sub-Áreas</a>
+                  <div className="dropdown-item create-btn">
+                    <Link to="/subarea/create" className="btn btn-primary w-100">Gerir Sub-Áreas</Link>
                   </div>
                 </ul>
               </li>
-
-              <li class="nav-item">
-                <a class="nav-link text-secondary fs-6" href="/user/profile">Perfil</a>
+              <li className="nav-item">
+                <Link className="nav-link text-secondary fs-6" to="/user/profile">Perfil</Link>
               </li>
-              <li class="nav-item">
-                <a class="nav-link text-secondary fs-6" href="/infos">Informações E Avisos</a>
+              <li className="nav-item">
+                <Link className="nav-link text-secondary fs-6" to="/infos">Informações E Avisos</Link>
               </li>
             </ul>
-            <button class="btn btn-outline-danger mt-3" onClick={handleLogout}>Logout</button>
+            <button className="btn btn-outline-danger mt-3" onClick={handleLogout}>Logout</button>
           </div>
         </div>
       </div>
