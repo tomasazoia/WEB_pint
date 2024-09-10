d0bd13ec61e3c85ac921cdf4b210eae7bb1b5faa
Tomas Azoia <tomasazoiamorais@gmail.com>
2024-09-10 23:32:40 +0100

    bb

--- backend/src/models/database.js
+++ backend/src/models/database.js
@@ -7,10 +7,10 @@ const dialectOptions = {};
       };
 
 const sequelize = new Sequelize({
-    database: 'pint_9w7c', 
-    username: 'pint_9w7c_user', 
-    password: 'uW1zulhr7o2kDzQXwIW0jOme2tWyU8yD', 
-    host: 'dpg-crbkikjtq21c73ck2q6g-a.frankfurt-postgres.render.com', 
+    database: 'database_pint', 
+    username: 'database_pint_user', 
+    password: '7QueNRqPOwYrAclwSNHLrePHf6apOyGv', 
+    host: 'dpg-crgchfo8fa8c73aod6sg-a.frankfurt-postgres.render.com', 
     port: 5432, 
     dialect: 'postgres', 
     dialectOptions: {
